31c8d99babc450a273363308a15e5bb00fd91401
Trevor Bernard <trevor.s.bernard@gmail.com>
2016-07-19 23:33:58 -0700

    geocoding now looks up longetude and latitude with every restaurant saved, also fixed misspelling in db column for longitude

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,13 @@
 class Restaurant < ActiveRecord::Base
- 
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    result = address
+    result += " #{city}" if city
+    result += " #{state}" if state
+    result += " #{zip}" if zip
+    return result
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720053248) do
+ActiveRecord::Schema.define(version: 20160720062154) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160720053248) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.float    "latitude"
-    t.float    "longitute"
+    t.float    "longitude"
   end
 
   add_index "restaurants", ["name"], name: "index_restaurants_on_name"
